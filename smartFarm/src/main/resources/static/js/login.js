c64d63b690ec2379a3f177d6cdf10939f36dccdb
LeeSubokk <97279495+LeeSubokk@users.noreply.github.com>
2022-05-21 01:15:16 +0900

    추가

--- smartFarm/src/main/resources/static/js/login.js
+++ smartFarm/src/main/resources/static/js/login.js
@@ -1,65 +1,32 @@
-
-//로그인 ajax
-function actionLoginCheck() {
-	if ($('#user_id').val() == '') {
-		Swal.fire({
-			icon: 'error',
-			title: '',
-			text: '아이디를 입력하세요.',
-		})
-		return false;
-	}
-
-	if ($('#user_password').val() == '') {
-		Swal.fire({
-			icon: 'error',
-			title: '',
-			text: '비밀번호를 입력하세요.',
-		})
-		return false;
-	}
-$("#loginForm").submit();
-
-/** 
- var token = $("meta[name='_csrf']").attr("content");
- var header = $("meta[name='_csrf_header']").attr("content");
-
-
-	$.ajax({
-		type: "post",
-		url: "/loginCheck",
-		data: { "user_id": $('#user_id').val(), "user_password": $('#user_password').val() },
-		dataType: "json",
-		beforeSend : function(xhr){
-		xhr.setRequestHeader(header, token);
-		},
-		success: function(json) {
-			idCheckResult = json.result;
-
-			if (idCheckResult == 'true') {
-				$("#loginForm").submit();
-
-			} else {
-				Swal.fire({
-					icon: 'error',
-					title: '',
-					text: '비밀번호가 맞지 않습니다.',
-				})
-				//$("#user_password").val('');
-				$("#user_password").focus();
-			}
-		},
-		error: function(error) {
+$(document).ready(function() {
+	//로그인페이지 엔터 누트면 로그인버튼 누르기
+	$('#loginForm').keypress(function(event) {
+		if (event.which == 13) {
+			actionLoginCheck();
+			return false;
+		}
+	})
+});
+	//로그인 ajax
+	function actionLoginCheck() {
+		if ($('#user_id').val() == '') {
 			Swal.fire({
 				icon: 'error',
 				title: '',
-				text: '아이디가 존재하지 않습니다.',
+				text: '아이디를 입력하세요.',
 			})
-			//$("#user_id").val('');
-			//$("#user_password").val('');
-			$("#user_id").focus();
+			return false;
 		}
-	});
 
-*/
-}
+		if ($('#user_password').val() == '') {
+			Swal.fire({
+				icon: 'error',
+				title: '',
+				text: '비밀번호를 입력하세요.',
+			})
+			return false;
+		}
+		$("#loginForm").submit();
+
+
+	}
